cdaf844f302f19eaa5e5d54cd09932f925598496
manojpothula <manojpothula350@gmail.com>
2026-08-25 00:08:11 +0530

    square of sorted array code added

---
+++ Two_Pointer/squares_of_sorted_arr.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+       int i = 0;
+       int j = nums.size()-1;
+       int n = nums.size();
+       int k = nums.size()-1;
+       vector<int>ans(n);
+       while(i<=j)
+       {
+         int left = nums[i] * nums[i];
+         int right = nums[j] * nums[j];
+         if(left > right)
+         {
+            ans[k]=left;
+            i++;
+         }
+         else
+         {
+            ans[k]=right;
+            j--;
+         }
+         k--;
+       }
+       return ans;
+    }
+};
